e3fd10aca89cffc7bd560dc6f3e63789ed3f329e
Sachkov <sachkov@live.com>
2022-09-19 17:40:38 +0400

    docker-compose fix

--- docker/app.sh
+++ docker/app.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 docker exec mello_app bash ./docker/composer_update.sh
+
+if ! [ -f vendor ]; then
+chown -R $HOST_UID:www-data vendor
+fi
--- docker/composer_update.sh
+++ docker/composer_update.sh
@@ -5,4 +5,3 @@ export COMPOSER_MEMORY_LIMIT=-1
 php -d memory_limit=-1 /usr/local/bin/composer update
 chown -R :www-data /var/www/html
 chmod -R 777 /var/www/html/storage
-chown -R $HOST_UID:www-data vendor
