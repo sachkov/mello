e4f6a4d98dc8642be91c6d96aaed7c675b074ff4
Sachkov <sachkov@live.com>
2022-09-19 17:27:44 +0400

    docker-compose fix

--- docker/composer_update.sh
+++ docker/composer_update.sh
@@ -2,7 +2,7 @@
 export HOST_UID=`id -u`
 export COMPOSER_MEMORY_LIMIT=-1
 
+php -d memory_limit=-1 /usr/local/bin/composer update
 chown -R :www-data /var/www/html
 chmod -R 777 /var/www/html/storage
-php -d memory_limit=-1 /usr/local/bin/composer update
 chown -R $HOST_UID:www-data vendor
